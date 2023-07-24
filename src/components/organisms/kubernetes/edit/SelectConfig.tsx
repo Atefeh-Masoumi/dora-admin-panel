2e567db27a187d9ce7042f492caeb3fd50848498
Alireza Shajari <a.shajari@bahartel.ir>
2023-07-24 10:32:10 +0330

    Update SelectConfig.tsx

--- src/components/organisms/kubernetes/edit/SelectConfig.tsx
+++ src/components/organisms/kubernetes/edit/SelectConfig.tsx
@@ -1,74 +1,44 @@
-import { FC, useContext, useEffect, useMemo } from "react";
-import { Box, Divider, Stack, Typography } from "@mui/material";
+import { FC, useContext, useEffect, useMemo, useState } from "react";
+import { Paper, Stack, Typography } from "@mui/material";
 import { LoadingButton } from "@mui/lab";
-import Grid2 from "@mui/material/Unstable_Grid2";
 import { toast } from "react-toastify";
-import { useNavigate } from "react-router";
-import { BaseTable } from "src/components/organisms/tables/BaseTable";
-import { PRODUCT_CATEGORY_ENUM } from "src/constant/productCategoryEnum";
 import { priceToPersian } from "src/utils/priceToPersian";
-import { EditWorkspaceTableRow } from "./tables/EditWorkspaceTableRow";
-import { editWorkspaceTableStruct } from "./tables/struct";
 import {
-  useGetPortalPanelProductBundleListByProductCategoryIdQuery,
   usePutPortalKubeWorkspaceEditMutation,
+  useGetPortalKubeHostGetByIdQuery,
 } from "src/app/services/api.generated";
 import { EditWorkspaceContext } from "./context/EditWorkspaceContext";
+import ReverseSlider from "src/components/atoms/ReverseSlider";
 
 const memoryUnitPrice = 600000;
 const cpuUnitPrice = 500000;
 const diskUnitPrice = 45000;
 const ipAddress = 600000;
 
-type ServerConfigPropsType = {};
+type SelectConfigPropsType = {};
 
-export const ServerConfig: FC<ServerConfigPropsType> = () => {
-  const { data: configsList, isLoading } =
-    useGetPortalPanelProductBundleListByProductCategoryIdQuery({
-      productCategoryId: PRODUCT_CATEGORY_ENUM.KUBE,
-    });
-
-  const table = useMemo(
-    () => (
-      <BaseTable
-        struct={editWorkspaceTableStruct}
-        RowComponent={EditWorkspaceTableRow}
-        rows={configsList || []}
-        text=""
-        isLoading={isLoading}
-      />
-    ),
-    [configsList, isLoading]
-  );
-
-  const navigate = useNavigate();
-
-  const [editWorkspaceHostModel, { isLoading: updateLoading }] =
-    usePutPortalKubeWorkspaceEditMutation();
+export const SelectConfig: FC<SelectConfigPropsType> = () => {
 
   const { serverId, serverConfig } = useContext(EditWorkspaceContext);
+  
   const [memory, setMemory] = useState(1);
   const [cpu, setCpu] = useState(1);
   const [disk, setDisk] = useState(25);
 
-  const [getData] = useLazyGetPortalVmVmGetByIdQuery();
+  const { data, isLoading } = useGetPortalKubeHostGetByIdQuery({
+    id: serverId || 0,
+  });
 
   const [sendNewConfig, { isLoading: sendNewConfigLoading }] =
     usePutPortalKubeWorkspaceEditMutation();
 
   useEffect(() => {
-    if (serverId) {
-      getData({ id: serverId })
-        .unwrap()
-        .then((res) => {
-          if (res) {
-            setMemory(res.memory || 0);
-            setCpu(res.cpu || 0);
-            setDisk(res.disk || 0);
-          }
-        });
+    if (serverId && data) {
+      setMemory(data.memory || 0);
+      setCpu(data.cpu || 0);
+      setDisk(data.disk || 0);
     }
-  }, [getData, serverId]);
+  }, [data, serverId]);
 
   const resourceList = [
     {
@@ -107,7 +77,7 @@ export const ServerConfig: FC<ServerConfigPropsType> = () => {
   const submitClickHandler = () => {
     if (!serverId) return;
     sendNewConfig({
-      editVmModel: {
+      editWorkspaceModel: {
         cpu,
         memory,
         disk,
@@ -188,108 +158,3 @@ export const ServerConfig: FC<ServerConfigPropsType> = () => {
     </>
   );
 };
-
-export const SelectConfig: FC<SelectConfigPropsType> = () => {
-  const { data: configsList, isLoading } =
-    useGetPortalPanelProductBundleListByProductCategoryIdQuery({
-      productCategoryId: PRODUCT_CATEGORY_ENUM.KUBE,
-    });
-
-  const table = useMemo(
-    () => (
-      <BaseTable
-        struct={editWorkspaceTableStruct}
-        RowComponent={EditWorkspaceTableRow}
-        rows={configsList || []}
-        text=""
-        isLoading={isLoading}
-      />
-    ),
-    [configsList, isLoading]
-  );
-
-  const navigate = useNavigate();
-
-  const { serverId, serverConfig } = useContext(EditWorkspaceContext);
-  const [editWorkspaceHostModel, { isLoading: updateLoading }] =
-    usePutPortalKubeWorkspaceEditMutation();
-
-  const submitHandler = () => {
-    if (!serverId || !serverConfig) return;
-
-    editWorkspaceHostModel({
-      editWorkspaceModel: {
-        id: serverId,
-        productBundleId: serverConfig.id || 0,
-      },
-    })
-      .unwrap()
-      .then(() => {
-        navigate("/kubernetes");
-        toast.success("درخواست تغییر سرویس با موفقیت انجام شد");
-      });
-    return;
-  };
-
-  return (
-    <>
-      <Grid2 container spacing={3} alignItems="center" justifyContent="center">
-        <Grid2 xs={12}>
-          <Stack
-            bgcolor="white"
-            py={3}
-            px={3}
-            width="100%"
-            borderRadius={3}
-            direction="column"
-          >
-            <Stack
-              direction={{ xs: "column", md: "row" }}
-              justifyContent="space-between"
-              alignItems="center"
-              rowGap={3}
-            >
-              <Stack
-                direction={{ xs: "column", sm: "row" }}
-                alignItems="center"
-                spacing={2}
-              >
-                <Typography fontSize={18} color="secondary">
-                  مشخصات سرویس را انتخاب کنید
-                </Typography>
-              </Stack>
-            </Stack>
-            <Divider sx={{ width: "100%", color: "#6E768A14", py: 1 }} />
-            <Box sx={{ px: { lg: 5 }, pt: 5 }}>{table}</Box>
-            <Stack py={3} px={3} alignItems="center" justifyContent="center">
-              <LoadingButton
-                loading={updateLoading}
-                variant="contained"
-                onClick={submitHandler}
-                sx={{
-                  width: { xs: "100%", sm: "auto" },
-                  px: { sm: 8 },
-                  py: 2.1,
-                }}
-              >
-                تغییر سرویس
-              </LoadingButton>
-            </Stack>
-          </Stack>
-        </Grid2>
-      </Grid2>
-    </>
-  );
-};
-function useState(arg0: number): [any, any] {
-  throw new Error("Function not implemented.");
-}
-
-function useState(arg0: number): [any, any] {
-  throw new Error("Function not implemented.");
-}
-
-function useLazyGetPortalVmVmGetByIdQuery(): [any] {
-  throw new Error("Function not implemented.");
-}
-
