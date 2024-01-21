a5613e2a331d46f403bfc8e90caea9133c5239f9
mahdian <mahdian@dorsacloud.com>
2024-01-21 10:18:17 +0330

    feat(index.tsx): update app version to 1.0.4

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import { App } from "src/App";
 
 import { store } from "./app/store";
 
-console.log("APP VERSION => 1.0.3");
+console.log("APP VERSION => 1.0.4");
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
