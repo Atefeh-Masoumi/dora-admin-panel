2b339211d24838f2d29dd42ebbc0ca4cdf1b83f4
mahdian <mahdian@dorsacloud.com>
2024-01-21 10:17:42 +0330

    feat(setting -> index.tsx): minor change in UI

--- src/pages/portal/setting/Index.tsx
+++ src/pages/portal/setting/Index.tsx
@@ -1,13 +1,25 @@
 import type { FC } from "react";
-import { Stack } from "@mui/material";
+import { Box, Stack } from "@mui/material";
 import { ChangePassword } from "src/components/organisms/portal/setting/ChangePassword";
 import { NotificationSetting } from "src/components/organisms/portal/setting/NotificationSetting";
+import { SecuritySetting } from "src/components/organisms/portal/setting/SecuritySetting";
 
 const Setting: FC = () => {
   return (
     <Stack direction="column" rowGap={2}>
       <ChangePassword />
-      <NotificationSetting />
+      <Stack
+        direction={{ xs: "column", md: "row" }}
+        // alignItems="center"
+        justifyContent="space-between"
+      >
+        <Box width={{ xs: "100%", md: "49%" }}>
+          <NotificationSetting />
+        </Box>
+        <Box width={{ xs: "100%", md: "49%" }}>
+          <SecuritySetting />
+        </Box>
+      </Stack>
     </Stack>
   );
 };
