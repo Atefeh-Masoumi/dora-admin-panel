f1e650110d63953206d86839cbc4fc0aee93d3c1
mahdian <mahdian@dorsacloud.com>
2024-01-23 20:04:20 +0330

    feat(authSlice.ts): add three new addMatcher

--- src/app/slice/authSlice.ts
+++ src/app/slice/authSlice.ts
@@ -85,10 +85,26 @@ const authSlice = createSlice({
     addMatcher(
       api.endpoints.putApiMyPortalProfileEdit.matchFulfilled,
       (state, actionPayload) => {
-        // const profileCompleted = actionPayload.(payload as any).profileCompleted;
-        console.log({ actionPayload });
-        const newState = { ...state };
-
+        const { profileCompleted } = actionPayload.payload;
+        const newState = { ...state, profileCompleted };
+        localStorage.setItem("loginInfo", JSON.stringify(newState));
+        return { ...newState, email: "", password: "" };
+      }
+    );
+    addMatcher(
+      api.endpoints.postApiMyPortalProfileConfirmPhoneNumber.matchFulfilled,
+      (state, actionPayload) => {
+        const { profileCompleted } = actionPayload.payload;
+        const newState = { ...state, profileCompleted };
+        localStorage.setItem("loginInfo", JSON.stringify(newState));
+        return { ...newState, email: "", password: "" };
+      }
+    );
+    addMatcher(
+      api.endpoints.postApiMyPortalProfileConfirmEmail.matchFulfilled,
+      (state, actionPayload) => {
+        const { profileCompleted } = actionPayload.payload;
+        const newState = { ...state, profileCompleted };
         localStorage.setItem("loginInfo", JSON.stringify(newState));
         return { ...newState, email: "", password: "" };
       }
