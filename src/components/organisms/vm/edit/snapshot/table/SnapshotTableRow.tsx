dd3ac6405145df0b9321065ef219e61ee486b45e
zahra <z.mivehchi@dorsa.cloud>
2024-05-15 17:07:49 +0330

    style(...): ...

--- src/components/organisms/vm/edit/snapshot/table/SnapshotTableRow.tsx
+++ src/components/organisms/vm/edit/snapshot/table/SnapshotTableRow.tsx
@@ -1,8 +1,8 @@
 import { FC, Fragment, useState } from "react";
 import { DorsaTableCell, DorsaTableRow } from "src/components/atoms/DorsaTable";
 import { snapShotTableStruct } from "./struct";
-import { Chip, IconButton, Stack } from "@mui/material";
-import { BORDER_RADIUS_1 } from "src/configs/theme";
+import { Chip, IconButton, Palette, Stack } from "@mui/material";
+import theme, { BORDER_RADIUS_1 } from "src/configs/theme";
 import { TrashSvg } from "src/components/atoms/svg-icons/TrashSvg";
 import { RefreshSvg } from "src/components/atoms/svg-icons/RefreshSvg";
 import { DeleteVmSnapshotDialog } from "../../../dialogs/DeleteVmSnapshotDialog";
@@ -18,14 +18,73 @@ export const SnapshotTableRow: FC<{ row: any }> = ({ row }) => {
   const handleOpenRevert = () => setOpenRevert(true);
   const handleCloseRevert = () => setOpenRevert(false);
 
+  const vmSnapshotStatusInfo = (vmSnapshotStatusId: number) => {
+    switch (vmSnapshotStatusId) {
+      case 1:
+        return {
+          id: 1,
+          label: "فعال",
+          bgcolor: theme.palette.success.light,
+          color: theme.palette.success.main,
+        };
+      case 2:
+        return {
+          id: 2,
+          label: "غیرفعال",
+          bgcolor: theme.palette.error.light,
+          color: theme.palette.error.main,
+        };
+      case 3:
+        return {
+          id: 3,
+          label: "درصف",
+          bgcolor: theme.palette.warning.light,
+          color: theme.palette.warning.main,
+        };
+
+      case 4:
+        return {
+          id: 4,
+          label: "درانتظار",
+          bgcolor: theme.palette.warning.light,
+          color: theme.palette.warning.main,
+        };
+
+      case 5:
+        return {
+          id: 5,
+          label: "ناموفق",
+          bgcolor: theme.palette.error.light,
+          color: theme.palette.error.main,
+        };
+
+      case 6:
+        return {
+          id: 6,
+          label: "حذف شده",
+          bgcolor: theme.palette.error.light,
+          color: theme.palette.error.main,
+        };
+      default:
+        return {
+          label: "نامشخص",
+          bgcolor: theme.palette.error.light,
+          color: theme.palette.error.main,
+        };
+    }
+  };
+
   return (
     <Fragment>
       <DorsaTableRow hover tabIndex={-1} key={row.value}>
         {snapShotTableStruct.map((column) => {
           const value = row[column.id];
           const text = column.format ? column.format(value) : value;
-          const isActive = row["isActive"];
-          const isCreated = row["isCreated"];
+          const isCreated = row.isCreated;
+          const vmSnapshotStatusId = row.vmSnapshotStatusId;
+
+          //TODO: for revert function
+          const vmTypeId = row.vmTypeId;
 
           return (
             <DorsaTableCell
@@ -35,7 +94,7 @@ export const SnapshotTableRow: FC<{ row: any }> = ({ row }) => {
             >
               {column.id === "control" ? (
                 <Stack direction="row" columnGap={1} alignItems="center">
-                  {!isActive && (
+                  {vmSnapshotStatusId === 2 && (
                     <IconButton onClick={handleOpenRevert}>
                       <RefreshSvg />
                     </IconButton>
@@ -45,14 +104,12 @@ export const SnapshotTableRow: FC<{ row: any }> = ({ row }) => {
                     <TrashSvg />
                   </IconButton>
                 </Stack>
-              ) : column.id === "isActive" ? (
+              ) : column.id === "vmSnapshotStatus" ? (
                 <Chip
-                  label={isActive ? "فعال" : "غیرفعال"}
+                  label={vmSnapshotStatusInfo(vmSnapshotStatusId).label}
                   sx={{
-                    bgcolor: ({ palette }) =>
-                      isActive ? palette.success.light : palette.error.light,
-                    color: ({ palette }) =>
-                      isActive ? palette.success.main : palette.error.main,
+                    bgcolor: vmSnapshotStatusInfo(vmSnapshotStatusId).bgcolor,
+                    color: vmSnapshotStatusInfo(vmSnapshotStatusId).color,
                     borderRadius: BORDER_RADIUS_1,
                   }}
                 />
--- src/components/organisms/vm/edit/snapshot/table/struct.ts
+++ src/components/organisms/vm/edit/snapshot/table/struct.ts
@@ -3,9 +3,9 @@ import { columnType } from "src/components/organisms/tables/table.types";
 export const snapShotTableStruct: columnType[] = [
   { id: "id", label: "شناسه" },
   { id: "name", label: "نام" },
-  { id: "isActive", label: "وضعیت فعال بودن" },
+  { id: "vmSnapshotStatus", label: "وضعیت" },
   { id: "isCreated", label: "وضعیت ایجاد" },
   { id: "description", label: "توضیحات" },
   { id: "createDate", label: "تاریخ ایجاد" },
-  { id: "control", label: "", disableSort: true },
+  { id: "control", label: "عملیات", disableSort: true },
 ];
