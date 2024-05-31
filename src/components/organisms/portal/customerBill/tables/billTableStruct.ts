d7b737c1b8a431ca8f850438d08aea8df40cd023
sasanrasouli <s.rasouli@dorsa.cloud>
2024-05-31 09:13:09 +0330

    update

--- src/components/organisms/portal/customerBill/tables/billTableStruct.ts
+++ src/components/organisms/portal/customerBill/tables/billTableStruct.ts
@@ -28,13 +28,12 @@ export const billTableStruct: columnType[] = [
 export const customerProductsTableStruct: columnType[] = [
   {
     id: "product",
-    label: "نام محصول",
+    label: "نام سرویس",
   },
   {
     id: "customerProduct",
     label: "نام محصول کاربر",
   },
-
   {
     id: "customerProductPrice",
     label: "مبلغ (ریال)",
@@ -56,9 +55,9 @@ export const customerProductsTableStruct: columnType[] = [
 
 export const customerProductItemsTableStruct: columnType[] = [
   { id: "customerProductItem", label: "نام" },
-  { id: "fromDate", label: "از تاریخ" },
-  { id: "toDate", label: "تا تاریخ" },
   { id: "quantity", label: "تعداد" },
   { id: "duration", label: "مدت استفاده" },
   { id: "price", label: "مبلغ" },
+  { id: "fromDate", label: "از تاریخ" },
+  { id: "toDate", label: "تا تاریخ" },
 ];
