fcc48a068282c257ee6d6988e3dd503c4d9ae754
zahra <z.mivehchi@dorsa.cloud>
2024-09-14 09:30:43 +0330

    syncApi and add nameSpaceId for deployment list

--- src/app/services/api.ts
+++ src/app/services/api.ts
@@ -452,7 +452,7 @@ export const api = generatedApi
         invalidatesTags: () => ["Namespace"],
       },
       // ============================== Kubernetes Cloud Deployment ============================== //
-      getApiMyKubernetesCloudDeploymentList: {
+      getApiMyKubernetesCloudDeploymentListByNamespaceId: {
         providesTags: () => ["Deployment"],
       },
       deleteApiMyKubernetesCloudDeploymentDeleteById: {
