3b13b02845b6633925b8118b7656f33a3a647ae3
“atefeh-masoumi” <a.masoumi@dorsa.cloud>
2024-12-23 14:58:06 +0330

    remove decoding base64 from secret key

--- src/components/organisms/kubernetesCloud/dialog/EditSecretMapDialog.tsx
+++ src/components/organisms/kubernetesCloud/dialog/EditSecretMapDialog.tsx
@@ -111,9 +111,9 @@ export const EditSecretMapDialog: FC<CreateVpcLoadBalancerDialogPropsType> = ({
       Object.keys(updatedConfigmap.envs).forEach((key) => {
         processedEnvsToObjectBase64[key] = {};
         Object.keys(updatedConfigmap.envs[key]).forEach((innerKey) => {
-          processedEnvsToObjectBase64[key][innerKey] = btoa(
+          processedEnvsToObjectBase64[key][innerKey] = 
             updatedConfigmap.envs[key][innerKey]
-          );
+          
         });
       });
 
