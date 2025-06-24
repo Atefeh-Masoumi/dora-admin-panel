f56ec16cabeb2517e5d4b9f3c7a64172307ee916
Atefeh Masoumi <atefeh.masoomy@gmail.com>
2025-06-24 22:17:51 +0330

    commenting some vpc features

--- src/components/organisms/vpc/add/steps/SelectVpcDataCenter.tsx
+++ src/components/organisms/vpc/add/steps/SelectVpcDataCenter.tsx
@@ -1,113 +1,113 @@
-import { Skeleton, Stack, Typography } from "@mui/material";
-import { FC, useContext } from "react";
-import {
-  DatacenterListResponse,
-  useGetApiMyDatacenterListQuery,
-} from "src/app/services/api.generated";
-import { BORDER_RADIUS_1 } from "src/configs/theme";
-import { AddVpcContext } from "../contexts/AddVpcContext";
-import DomainIcon from "@mui/icons-material/Domain";
+// import { Skeleton, Stack, Typography } from "@mui/material";
+// import { FC, useContext } from "react";
+// import {
+//   DatacenterListResponse,
+//   useGetApiMyDatacenterListQuery,
+// } from "src/app/services/api.generated";
+// import { BORDER_RADIUS_1 } from "src/configs/theme";
+// import { AddVpcContext } from "../contexts/AddVpcContext";
+// import DomainIcon from "@mui/icons-material/Domain";
 
-type SelectKuberDataCenterPropsType = {};
+// type SelectKuberDataCenterPropsType = {};
 
-export const SelectVpcDataCenter: FC<SelectKuberDataCenterPropsType> = () => {
-  const { dataCenter, setDataCenter } = useContext(AddVpcContext);
+// export const SelectVpcDataCenter: FC<SelectKuberDataCenterPropsType> = () => {
+//   const { dataCenter, setDataCenter } = useContext(AddVpcContext);
 
-  const { data: dataCenterList, isLoading } = useGetApiMyDatacenterListQuery();
+//   const { data: dataCenterList, isLoading } = useGetApiMyDatacenterListQuery();
 
-  const dataCenterOnClick = (dataCenter: DatacenterListResponse) => {
-    setDataCenter(dataCenter);
-  };
+//   const dataCenterOnClick = (dataCenter: DatacenterListResponse) => {
+//     setDataCenter(dataCenter);
+//   };
 
-  return (
-    <Stack justifyContent="center" alignItems="center" spacing={4}>
-      <Typography fontSize={24} fontWeight="bold" alignItems="center">
-        مرکز داده را انتخاب کنید
-      </Typography>
-      <Stack
-        justifyContent="center"
-        direction={{ xs: "column", sm: "row" }}
-        spacing={3}
-        sx={{ width: "100%" }}
-      >
-        {isLoading &&
-          [...Array(2)].map((_, index) => (
-            <Stack
-              key={index}
-              direction="row"
-              sx={{
-                width: { xs: "100%", sm: 200 },
-                height: 96,
-                transition: "150ms",
-                borderRadius: BORDER_RADIUS_1,
-                border: "1px solid rgba(110, 118, 138, 0.12)",
-                overflow: "hidden",
-                p: 1,
-                cursor: "pointer",
-              }}
-              alignItems="center"
-              justifyContent="center"
-              spacing={1}
-            >
-              <Skeleton variant="circular" width={44} height={44} />
-              <Skeleton width="30%" />
-            </Stack>
-          ))}
-        {dataCenterList &&
-          dataCenterList.map((dataCenterItem) => {
-            const { id, name } = dataCenterItem;
-            const isSelected = dataCenter ? dataCenter.id === id : false;
-            return (
-              <Stack
-                key={id}
-                direction="row"
-                sx={{
-                  width: { xs: "100%", sm: 200 },
-                  height: 96,
-                  transition: "150ms",
-                  borderRadius: BORDER_RADIUS_1,
-                  border: ({ palette }) =>
-                    `2px solid ${
-                      isSelected
-                        ? palette.primary.main
-                        : "rgba(110, 118, 138, 0.12)"
-                    }`,
-                  overflow: "hidden",
-                  p: 1,
-                  cursor: "pointer",
-                }}
-                alignItems="center"
-                justifyContent="center"
-                spacing={1}
-                onClick={() => dataCenterOnClick(dataCenterItem)}
-              >
-                <Stack
-                  alignItems="center"
-                  justifyContent="center"
-                  sx={{
-                    transition: "150ms",
-                    filter: isSelected ? "grayscale(0)" : "grayscale(100%)",
-                    height: "41px",
-                    overflow: "hidden",
-                    display: "flex",
-                    margin: "0 auto",
-                  }}
-                >
-                  <DomainIcon />
-                </Stack>
-                <Typography
-                  noWrap
-                  color={isSelected ? "primary.main" : "secondary.main"}
-                  sx={{ transition: "150ms" }}
-                  fontWeight="bold"
-                  fontSize={20}
-                >
-                  {name}
-                </Typography>
-              </Stack>
-            );
-          })}
-      </Stack>
-    </Stack>
-  );
-};
+//   return (
+//     <Stack justifyContent="center" alignItems="center" spacing={4}>
+//       <Typography fontSize={24} fontWeight="bold" alignItems="center">
+//         مرکز داده را انتخاب کنید
+//       </Typography>
+//       <Stack
+//         justifyContent="center"
+//         direction={{ xs: "column", sm: "row" }}
+//         spacing={3}
+//         sx={{ width: "100%" }}
+//       >
+//         {isLoading &&
+//           [...Array(2)].map((_, index) => (
+//             <Stack
+//               key={index}
+//               direction="row"
+//               sx={{
+//                 width: { xs: "100%", sm: 200 },
+//                 height: 96,
+//                 transition: "150ms",
+//                 borderRadius: BORDER_RADIUS_1,
+//                 border: "1px solid rgba(110, 118, 138, 0.12)",
+//                 overflow: "hidden",
+//                 p: 1,
+//                 cursor: "pointer",
+//               }}
+//               alignItems="center"
+//               justifyContent="center"
+//               spacing={1}
+//             >
+//               <Skeleton variant="circular" width={44} height={44} />
+//               <Skeleton width="30%" />
+//             </Stack>
+//           ))}
+//         {dataCenterList &&
+//           dataCenterList.map((dataCenterItem) => {
+//             const { id, name } = dataCenterItem;
+//             const isSelected = dataCenter ? dataCenter.id === id : false;
+//             return (
+//               <Stack
+//                 key={id}
+//                 direction="row"
+//                 sx={{
+//                   width: { xs: "100%", sm: 200 },
+//                   height: 96,
+//                   transition: "150ms",
+//                   borderRadius: BORDER_RADIUS_1,
+//                   border: ({ palette }) =>
+//                     `2px solid ${
+//                       isSelected
+//                         ? palette.primary.main
+//                         : "rgba(110, 118, 138, 0.12)"
+//                     }`,
+//                   overflow: "hidden",
+//                   p: 1,
+//                   cursor: "pointer",
+//                 }}
+//                 alignItems="center"
+//                 justifyContent="center"
+//                 spacing={1}
+//                 onClick={() => dataCenterOnClick(dataCenterItem)}
+//               >
+//                 <Stack
+//                   alignItems="center"
+//                   justifyContent="center"
+//                   sx={{
+//                     transition: "150ms",
+//                     filter: isSelected ? "grayscale(0)" : "grayscale(100%)",
+//                     height: "41px",
+//                     overflow: "hidden",
+//                     display: "flex",
+//                     margin: "0 auto",
+//                   }}
+//                 >
+//                   <DomainIcon />
+//                 </Stack>
+//                 <Typography
+//                   noWrap
+//                   color={isSelected ? "primary.main" : "secondary.main"}
+//                   sx={{ transition: "150ms" }}
+//                   fontWeight="bold"
+//                   fontSize={20}
+//                 >
+//                   {name}
+//                 </Typography>
+//               </Stack>
+//             );
+//           })}
+//       </Stack>
+//     </Stack>
+//   );
+// };
--- src/components/organisms/vpc/add/steps/SelectVpcHypervisor.tsx
+++ src/components/organisms/vpc/add/steps/SelectVpcHypervisor.tsx
@@ -1,124 +1,124 @@
-import { Skeleton, Stack, Typography } from "@mui/material";
-import { FC, useContext } from "react";
-import {
-  HypervisorListResponse,
-  useGetApiMyHostHypervisorListQuery,
-} from "src/app/services/api.generated";
-import openstackImage from "src/assets/images/openstack.png";
-import vmwareImage from "src/assets/images/vmwareLogo.png";
-import { BORDER_RADIUS_1 } from "src/configs/theme";
-import { AddVpcContext } from "../contexts/AddVpcContext";
+// import { Skeleton, Stack, Typography } from "@mui/material";
+// import { FC, useContext } from "react";
+// import {
+//   HypervisorListResponse,
+//   useGetApiMyHostHypervisorListQuery,
+// } from "src/app/services/api.generated";
+// import openstackImage from "src/assets/images/openstack.png";
+// import vmwareImage from "src/assets/images/vmwareLogo.png";
+// import { BORDER_RADIUS_1 } from "src/configs/theme";
+// import { AddVpcContext } from "../contexts/AddVpcContext";
 
-type SelectVpcHypervisorPropsType = {};
+// type SelectVpcHypervisorPropsType = {};
 
-export const SelectVpcHypervisor: FC<SelectVpcHypervisorPropsType> = () => {
-  const { hypervisor, setHypervisor, dataCenter } = useContext(AddVpcContext);
-  const isActive = Boolean(dataCenter?.id);
+// export const SelectVpcHypervisor: FC<SelectVpcHypervisorPropsType> = () => {
+//   const { hypervisor, setHypervisor, dataCenter } = useContext(AddVpcContext);
+//   const isActive = Boolean(dataCenter?.id);
 
-  const { data: hypervisorList, isLoading } =
-    useGetApiMyHostHypervisorListQuery();
+//   const { data: hypervisorList, isLoading } =
+//     useGetApiMyHostHypervisorListQuery();
 
-  const dataCenterOnClick = (hypervisor: HypervisorListResponse) => {
-    setHypervisor(hypervisor);
-  };
+//   const dataCenterOnClick = (hypervisor: HypervisorListResponse) => {
+//     setHypervisor(hypervisor);
+//   };
 
-  return (
-    <Stack justifyContent="center" alignItems="center" spacing={4}>
-      <Typography fontSize={24} fontWeight="bold" alignItems="center">
-        زیرساخت را انتخاب کنید
-      </Typography>
-      <Stack
-        justifyContent="center"
-        direction={{ xs: "column", sm: "row" }}
-        spacing={3}
-        sx={{ width: "100%" }}
-      >
-        {isLoading &&
-          [...Array(2)].map((_, index) => (
-            <Stack
-              key={index}
-              direction="row"
-              sx={{
-                width: { xs: "100%", sm: 200 },
-                height: 96,
-                transition: "150ms",
-                borderRadius: BORDER_RADIUS_1,
-                border: "1px solid rgba(110, 118, 138, 0.12)",
-                overflow: "hidden",
-                p: 1,
-                cursor: "pointer",
-              }}
-              alignItems="center"
-              justifyContent="center"
-              spacing={1}
-            >
-              <Skeleton variant="circular" width={44} height={44} />
-              <Skeleton width="30%" />
-            </Stack>
-          ))}
-        {hypervisorList &&
-          hypervisorList.map((hypervisorItem) => {
-            const { id, name } = hypervisorItem;
-            const isSelected = hypervisor ? hypervisor.id === id : false;
-            return (
-              <Stack
-                key={id}
-                direction="row"
-                sx={{
-                  width: { xs: "100%", sm: 200 },
-                  height: 96,
-                  transition: "150ms",
-                  borderRadius: BORDER_RADIUS_1,
-                  border: ({ palette }) =>
-                    `2px solid ${
-                      isSelected && isActive
-                        ? palette.primary.main
-                        : "rgba(110, 118, 138, 0.12)"
-                    }`,
-                  overflow: "hidden",
-                  p: 1,
-                  cursor: isActive ? "pointer" : "not-allowed",
-                  opacity: isActive ? 1 : 0.5,
-                }}
-                alignItems="center"
-                justifyContent="center"
-                spacing={1}
-                onClick={() => dataCenterOnClick(hypervisorItem)}
-              >
-                <Stack
-                  alignItems="center"
-                  justifyContent="center"
-                  sx={{
-                    transition: "150ms",
-                    filter: isSelected ? "grayscale(0)" : "grayscale(100%)",
-                    height: "41px",
-                    overflow: "hidden",
-                  }}
-                >
-                  <img
-                    src={id === 1 ? vmwareImage : openstackImage}
-                    alt=""
-                    style={{
-                      verticalAlign: "center",
-                      width: "100%",
-                      height: "100%",
-                    }}
-                  />
-                </Stack>
-                <Typography
-                  noWrap
-                  color={
-                    isSelected && isActive ? "primary.main" : "secondary.main"
-                  }
-                  sx={{ transition: "150ms" }}
-                  fontWeight="bold"
-                >
-                  {name}
-                </Typography>
-              </Stack>
-            );
-          })}
-      </Stack>
-    </Stack>
-  );
-};
+//   return (
+//     <Stack justifyContent="center" alignItems="center" spacing={4}>
+//       <Typography fontSize={24} fontWeight="bold" alignItems="center">
+//         زیرساخت را انتخاب کنید
+//       </Typography>
+//       <Stack
+//         justifyContent="center"
+//         direction={{ xs: "column", sm: "row" }}
+//         spacing={3}
+//         sx={{ width: "100%" }}
+//       >
+//         {isLoading &&
+//           [...Array(2)].map((_, index) => (
+//             <Stack
+//               key={index}
+//               direction="row"
+//               sx={{
+//                 width: { xs: "100%", sm: 200 },
+//                 height: 96,
+//                 transition: "150ms",
+//                 borderRadius: BORDER_RADIUS_1,
+//                 border: "1px solid rgba(110, 118, 138, 0.12)",
+//                 overflow: "hidden",
+//                 p: 1,
+//                 cursor: "pointer",
+//               }}
+//               alignItems="center"
+//               justifyContent="center"
+//               spacing={1}
+//             >
+//               <Skeleton variant="circular" width={44} height={44} />
+//               <Skeleton width="30%" />
+//             </Stack>
+//           ))}
+//         {hypervisorList &&
+//           hypervisorList.map((hypervisorItem) => {
+//             const { id, name } = hypervisorItem;
+//             const isSelected = hypervisor ? hypervisor.id === id : false;
+//             return (
+//               <Stack
+//                 key={id}
+//                 direction="row"
+//                 sx={{
+//                   width: { xs: "100%", sm: 200 },
+//                   height: 96,
+//                   transition: "150ms",
+//                   borderRadius: BORDER_RADIUS_1,
+//                   border: ({ palette }) =>
+//                     `2px solid ${
+//                       isSelected && isActive
+//                         ? palette.primary.main
+//                         : "rgba(110, 118, 138, 0.12)"
+//                     }`,
+//                   overflow: "hidden",
+//                   p: 1,
+//                   cursor: isActive ? "pointer" : "not-allowed",
+//                   opacity: isActive ? 1 : 0.5,
+//                 }}
+//                 alignItems="center"
+//                 justifyContent="center"
+//                 spacing={1}
+//                 onClick={() => dataCenterOnClick(hypervisorItem)}
+//               >
+//                 <Stack
+//                   alignItems="center"
+//                   justifyContent="center"
+//                   sx={{
+//                     transition: "150ms",
+//                     filter: isSelected ? "grayscale(0)" : "grayscale(100%)",
+//                     height: "41px",
+//                     overflow: "hidden",
+//                   }}
+//                 >
+//                   <img
+//                     src={id === 1 ? vmwareImage : openstackImage}
+//                     alt=""
+//                     style={{
+//                       verticalAlign: "center",
+//                       width: "100%",
+//                       height: "100%",
+//                     }}
+//                   />
+//                 </Stack>
+//                 <Typography
+//                   noWrap
+//                   color={
+//                     isSelected && isActive ? "primary.main" : "secondary.main"
+//                   }
+//                   sx={{ transition: "150ms" }}
+//                   fontWeight="bold"
+//                 >
+//                   {name}
+//                 </Typography>
+//               </Stack>
+//             );
+//           })}
+//       </Stack>
+//     </Stack>
+//   );
+// };
