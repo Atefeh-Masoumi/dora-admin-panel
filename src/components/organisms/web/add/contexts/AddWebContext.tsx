a204822e310341efa63535baebe57143bfac9fe4
atefeh <a.masoumi@dorsa.cloud>
2025-06-29 12:49:59 +0330

    refactor: comment out unused imports and component code in AddStepper, AddWebContext, SelectConfig, SelectDataCenter, SelectDomain, and related components

--- src/components/organisms/web/add/contexts/AddWebContext.tsx
+++ src/components/organisms/web/add/contexts/AddWebContext.tsx
@@ -1,71 +1,71 @@
-import { FC, createContext, ReactNode, useState } from "react";
-import {
-  DatacenterListResponse,
-  ProductBundleWebListResponse,
-} from "src/app/services/api.generated";
+// import { FC, createContext, ReactNode, useState } from "react";
+// import {
+//   DatacenterListResponse,
+//   ProductBundleWebListResponse,
+// } from "src/app/services/api.generated";
 
-export type addWebStepsType = 1 | 2 | 3 | 4;
+// export type addWebStepsType = 1 | 2 | 3 | 4;
 
-type AddWebContextType = {
-  step: addWebStepsType;
-  setStep: (step: addWebStepsType) => void;
-  domainName: string;
-  setDomainName: (domainName: string) => void;
-  dataCenter: DatacenterListResponse | null;
-  setDataCenter: (dataCenter: DatacenterListResponse | null) => void;
-  serverConfig: ProductBundleWebListResponse | null;
-  setServerConfig: (osConfig: ProductBundleWebListResponse | null) => void;
-  term: boolean;
-  setTerm: (term: boolean) => void;
-};
+// type AddWebContextType = {
+//   step: addWebStepsType;
+//   setStep: (step: addWebStepsType) => void;
+//   domainName: string;
+//   setDomainName: (domainName: string) => void;
+//   dataCenter: DatacenterListResponse | null;
+//   setDataCenter: (dataCenter: DatacenterListResponse | null) => void;
+//   serverConfig: ProductBundleWebListResponse | null;
+//   setServerConfig: (osConfig: ProductBundleWebListResponse | null) => void;
+//   term: boolean;
+//   setTerm: (term: boolean) => void;
+// };
 
-export const AddWebContext = createContext<AddWebContextType>({
-  step: 1,
-  setStep: (step) => {},
-  domainName: "",
-  setDomainName: (domainName) => {},
-  dataCenter: null,
-  setDataCenter: (dataCenter) => {},
-  serverConfig: null,
-  setServerConfig: (serverConfig) => {},
-  term: false,
-  setTerm: (term) => {},
-});
+// export const AddWebContext = createContext<AddWebContextType>({
+//   step: 1,
+//   setStep: (step) => {},
+//   domainName: "",
+//   setDomainName: (domainName) => {},
+//   dataCenter: null,
+//   setDataCenter: (dataCenter) => {},
+//   serverConfig: null,
+//   setServerConfig: (serverConfig) => {},
+//   term: false,
+//   setTerm: (term) => {},
+// });
 
-type AddWebContextProviderPropsType = {
-  children?: ReactNode;
-};
+// type AddWebContextProviderPropsType = {
+//   children?: ReactNode;
+// };
 
-const AddWebContextProvider: FC<AddWebContextProviderPropsType> = ({
-  children,
-}) => {
-  const [step, setStep] = useState<addWebStepsType>(1);
-  const [domainName, setDomainName] = useState("");
-  const [dataCenter, setDataCenter] = useState<DatacenterListResponse | null>(
-    null
-  );
-  const [serverConfig, setServerConfig] =
-    useState<ProductBundleWebListResponse | null>(null);
-  const [term, setTerm] = useState(false);
+// const AddWebContextProvider: FC<AddWebContextProviderPropsType> = ({
+//   children,
+// }) => {
+//   const [step, setStep] = useState<addWebStepsType>(1);
+//   const [domainName, setDomainName] = useState("");
+//   const [dataCenter, setDataCenter] = useState<DatacenterListResponse | null>(
+//     null
+//   );
+//   const [serverConfig, setServerConfig] =
+//     useState<ProductBundleWebListResponse | null>(null);
+//   const [term, setTerm] = useState(false);
 
-  return (
-    <AddWebContext.Provider
-      value={{
-        step,
-        setStep,
-        domainName,
-        setDomainName,
-        dataCenter,
-        setDataCenter,
-        serverConfig,
-        setServerConfig,
-        term,
-        setTerm,
-      }}
-    >
-      {children}
-    </AddWebContext.Provider>
-  );
-};
+//   return (
+//     <AddWebContext.Provider
+//       value={{
+//         step,
+//         setStep,
+//         domainName,
+//         setDomainName,
+//         dataCenter,
+//         setDataCenter,
+//         serverConfig,
+//         setServerConfig,
+//         term,
+//         setTerm,
+//       }}
+//     >
+//       {children}
+//     </AddWebContext.Provider>
+//   );
+// };
 
-export default AddWebContextProvider;
+// export default AddWebContextProvider;
--- src/components/organisms/web/edit/tables/EditWebTableRow.tsx
+++ src/components/organisms/web/edit/tables/EditWebTableRow.tsx
@@ -1,51 +1,51 @@
-import { FC, useContext, useMemo } from "react";
-import { Checkbox } from "@mui/material";
-import { DorsaTableCell, DorsaTableRow } from "src/components/atoms/DorsaTable";
-import { editWebTableStruct } from "./editWebTableStruct";
-import { EditWebContext } from "../contexts/EditWebContext";
-import { withTableRowWrapper } from "src/HOC/withTableRowWrapper";
+// import { FC, useContext, useMemo } from "react";
+// import { Checkbox } from "@mui/material";
+// import { DorsaTableCell, DorsaTableRow } from "src/components/atoms/DorsaTable";
+// import { editWebTableStruct } from "./editWebTableStruct";
+// import { EditWebContext } from "../contexts/EditWebContext";
+// import { withTableRowWrapper } from "src/HOC/withTableRowWrapper";
 
-const EditWebTableRow: FC<{ row: any }> = ({ row }) => {
-  const { serverConfig, setServerConfig } = useContext(EditWebContext);
+// const EditWebTableRow: FC<{ row: any }> = ({ row }) => {
+//   const { serverConfig, setServerConfig } = useContext(EditWebContext);
 
-  const isChecked = useMemo(
-    () => serverConfig?.id === row["id"],
-    [row, serverConfig?.id]
-  );
+//   const isChecked = useMemo(
+//     () => serverConfig?.id === row["id"],
+//     [row, serverConfig?.id]
+//   );
 
-  const onCheckboxClick = () => {
-    if (serverConfig?.id === row["id"]) {
-      setServerConfig(null);
-      return;
-    }
-    setServerConfig(row);
-  };
+//   const onCheckboxClick = () => {
+//     if (serverConfig?.id === row["id"]) {
+//       setServerConfig(null);
+//       return;
+//     }
+//     setServerConfig(row);
+//   };
 
-  return (
-    <DorsaTableRow hover tabIndex={-1} key={row.value}>
-      <DorsaTableCell padding="checkbox" onClick={onCheckboxClick}>
-        <Checkbox
-          sx={{ borderRadius: "6px" }}
-          color="primary"
-          checked={isChecked}
-          onChange={onCheckboxClick}
-        />
-      </DorsaTableCell>
-      {editWebTableStruct.slice(1, editWebTableStruct.length).map((column) => {
-        const value = row[column.id];
-        const text = column.format ? column.format(value) : value;
-        return (
-          <DorsaTableCell
-            key={column.id}
-            align="center"
-            sx={{ px: 1, whiteSpace: "nowrap" }}
-          >
-            {text}
-          </DorsaTableCell>
-        );
-      })}
-    </DorsaTableRow>
-  );
-};
+//   return (
+//     <DorsaTableRow hover tabIndex={-1} key={row.value}>
+//       <DorsaTableCell padding="checkbox" onClick={onCheckboxClick}>
+//         <Checkbox
+//           sx={{ borderRadius: "6px" }}
+//           color="primary"
+//           checked={isChecked}
+//           onChange={onCheckboxClick}
+//         />
+//       </DorsaTableCell>
+//       {editWebTableStruct.slice(1, editWebTableStruct.length).map((column) => {
+//         const value = row[column.id];
+//         const text = column.format ? column.format(value) : value;
+//         return (
+//           <DorsaTableCell
+//             key={column.id}
+//             align="center"
+//             sx={{ px: 1, whiteSpace: "nowrap" }}
+//           >
+//             {text}
+//           </DorsaTableCell>
+//         );
+//       })}
+//     </DorsaTableRow>
+//   );
+// };
 
-export default withTableRowWrapper(EditWebTableRow);
+// export default withTableRowWrapper(EditWebTableRow);
