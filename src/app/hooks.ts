aeed6f23705e0e9a74f1b771ffe1e1b55a265aa2
atefeh <a.masoumi@dorsa.cloud>
2025-06-29 16:12:17 +0330

    refactor: add shallow equal selector and project management features in Header component for improved state handling and user experience

--- src/app/hooks.ts
+++ src/app/hooks.ts
@@ -1,6 +1,11 @@
-import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
+import { shallowEqual, TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 import type { RootStateType, AppDispatch } from "./store";
 
+type Selector<T> = (state: RootStateType) => T;
 // Use throughout your app instead of plain `useDispatch` and `useSelector`
 export const useAppDispatch = () => useDispatch<AppDispatch>();
 export const useAppSelector: TypedUseSelectorHook<RootStateType> = useSelector;
+
+export const useShallowEqualSelector = <T>(selector: Selector<T>): T => {
+return useSelector(selector, shallowEqual);
+};
