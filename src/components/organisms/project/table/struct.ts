aa1e7d9afb02920193320af6645f4aa48d80ad03
atefeh <a.masoumi@dorsa.cloud>
2025-07-01 16:16:55 +0330

    refactor: update ProjectUsers component to use projectId from useParams and enhance user access type display in project user table

--- src/components/organisms/project/table/struct.ts
+++ src/components/organisms/project/table/struct.ts
@@ -1,10 +1,27 @@
 import { columnType } from "src/components/organisms/tables/table.types";
 
+const getUserAccessTypeName = (accessTypeId: number | undefined) => {
+  switch (accessTypeId) {
+    case 1:
+      return "Owner";
+    case 2:
+      return "Maintainer";
+    case 3:
+      return "Reporter";
+    default:
+      return "-";
+  }
+};
+
 export const projectUserTableStruct: columnType[] = [
   { id: "id", label: "" },
   { id: "user", label: "کاربر" },
   { id: "email", label: "ایمیل" },
-  { id: "role", label: "نقش" },
+  { 
+    id: "userAccessTypeId", 
+    label: "نوع دسترسی",
+    format: getUserAccessTypeName
+  },
   { id: "createDate", label: "تاریخ اضافه شدن" },
   { id: "control", label: "", disableSort: true },
 ]; 
