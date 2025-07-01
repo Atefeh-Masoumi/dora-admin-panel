5255834e7fdde23de67614efa3688b067e7f7e67
atefeh <a.masoumi@dorsa.cloud>
2025-07-01 16:16:33 +0330

    refactor: restructure ProjectInfo component layout using Grid for improved responsiveness and readability

--- src/components/organisms/project/ProjectInfo.tsx
+++ src/components/organisms/project/ProjectInfo.tsx
@@ -1,9 +1,11 @@
 import { FC } from "react";
 import { useParams } from "react-router-dom";
-import { Paper, Stack, Typography, Chip } from "@mui/material";
+import { Paper, Stack, Typography, Chip, Grid, Divider } from "@mui/material";
 import { useGetApiMyProjectGetByIdQuery } from "src/app/services/api.generated";
 import { BORDER_RADIUS_1 } from "src/configs/theme";
 import { e2p } from "src/utils/e2p.utils";
+import Moment from "src/utils/utils";
+import { ConvertToJalali } from "src/utils/convertToJalali";
 type ProjectInfoPropsType = {};
 
 export const ProjectInfo: FC<ProjectInfoPropsType> = () => {
@@ -23,40 +25,69 @@ export const ProjectInfo: FC<ProjectInfoPropsType> = () => {
       >
         مشخصات پروژه
       </Typography>
-      <Paper
-        elevation={0}
-        sx={{ overflow: "hidden", px: { xs: 2, sm: 3, md: 4, lg: 5 }, py: 5 }}
-      >
-        {isLoading ? (
-          <Typography>در حال بارگذاری...</Typography>
-        ) : (
-          <Stack spacing={3}>
-            <Stack direction="row" spacing={2} alignItems="center">
-              <Typography fontWeight="bold">نام پروژه:</Typography>
-              <Typography>{projectData?.name || "--"}</Typography>
-            </Stack>
-            
-            <Stack direction="row" spacing={2} alignItems="center">
-              <Typography fontWeight="bold">نوع پروژه:</Typography>
-              <Chip
-                label={projectData?.isPublic ? "عمومی" : "خصوصی"}
-                color={projectData?.isPublic ? "success" : "error"}
-                sx={{ borderRadius: BORDER_RADIUS_1 }}
-              />
-            </Stack>
-            
-            <Stack direction="row" spacing={2} alignItems="center">
-              <Typography fontWeight="bold">مرکز داده:</Typography>
-              <Typography>{projectData?.datacenterName || "--"}</Typography>
-            </Stack>
-            
-            <Stack direction="row" spacing={2} alignItems="center">
-              <Typography fontWeight="bold">تاریخ ایجاد:</Typography>
-              <Typography>{projectData?.createDate ? e2p(projectData?.createDate) : "--"}</Typography>
-            </Stack>
-          </Stack>
-        )}
-      </Paper>
+      <Grid container>
+        <Grid
+          container
+          spacing={3}
+          justifyContent="space-between"
+          alignItems="stretch"
+          width="100%"
+        >
+          <Grid item xs={12}>
+            <Paper
+              component={Stack}
+              rowGap={2}
+              elevation={0}
+              sx={{
+                borderRadius: BORDER_RADIUS_1,
+                p: { xs: 2.5 },
+                height: "100%",
+              }}
+            >
+              <Typography fontWeight="bold" fontSize={18}>
+              </Typography>
+              {/* <Divider /> */}
+              {isLoading ? (
+                <Typography>در حال بارگذاری...</Typography>
+              ) : (
+                <Grid container spacing={3}>
+                  <Grid item xs={12} md={6} display={"flex"} flexDirection={"column"}>
+                    <Stack spacing={2}>
+                      <Stack direction="row" spacing={2} alignItems="center">
+                        <Typography fontWeight="bold">نام پروژه:</Typography>
+                        <Typography>{projectData?.name || "--"}</Typography>
+                      </Stack>
+                      
+                      <Stack direction="row" spacing={2} alignItems="center">
+                        <Typography fontWeight="bold">نوع پروژه:</Typography>
+                        <Chip
+                          label={projectData?.isPublic ? "عمومی" : "خصوصی"}
+                          color={projectData?.isPublic ? "success" : "error"}
+                          sx={{ borderRadius: BORDER_RADIUS_1 }}
+                        />
+                      </Stack>
+                    </Stack>
+                  </Grid>
+                  
+                  <Grid item xs={12} md={6} display={"flex"} flexDirection={"column"}>
+                    <Stack spacing={2}>
+                      <Stack direction="row" spacing={2} alignItems="center">
+                        <Typography fontWeight="bold">مرکز داده:</Typography>
+                        <Typography>{projectData?.datacenterName || "--"}</Typography>
+                      </Stack>
+                      
+                      <Stack direction="row" spacing={2} alignItems="center">
+                        <Typography fontWeight="bold">تاریخ ایجاد:</Typography>
+                        <Typography>{projectData?.createDate ? e2p(ConvertToJalali(projectData?.createDate)) : "--"}</Typography>
+                      </Stack>
+                    </Stack>
+                  </Grid>
+                </Grid>
+              )}
+            </Paper>
+          </Grid>
+        </Grid>
+      </Grid>
     </>
   );
 }; 
