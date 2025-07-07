9baeb05f262e50c92b7fcdebb26c6b3da0654903
atefeh <a.masoumi@dorsa.cloud>
2025-07-07 14:39:16 +0330

    fix(query): add error handling for validation errors with specific message for status 422

--- src/app/services/baseQuery.ts
+++ src/app/services/baseQuery.ts
@@ -79,6 +79,10 @@ export const baseQuery: BaseQueryFn<
       toast.error(error.errorMessage || defaultErrorMessage);
       return { error };
     }
+    if (error.status == 422){
+      toast.error("اطلاعات وارد شده معتبر نمی باشد");
+      return { error };
+    }
     if (error.status === 403) {
       navigateTo("/forbidden");
       return { error };
@@ -94,7 +98,7 @@ export const baseQuery: BaseQueryFn<
       toast.error(error.errorMessage || defaultErrorMessage);
       return { error };
     }
-
+  
     toast.error(error.errorMessage || `\n ${defaultErrorMessage}`);
 
     return { error };
