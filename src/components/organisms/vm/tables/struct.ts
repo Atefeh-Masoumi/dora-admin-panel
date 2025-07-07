2260b3905e63e4c49fca7bcb8541ccb1090fdc1f
atefeh <a.masoumi@dorsa.cloud>
2025-07-07 11:43:22 +0330

    refactor(struct): remove IPv4 column from VM table structure

--- src/components/organisms/vm/tables/struct.ts
+++ src/components/organisms/vm/tables/struct.ts
@@ -4,7 +4,6 @@ export const addVmTableStruct: columnType[] = [
   { id: "id", label: "", invisibility: true },
   { id: "name", label: "نام ماشین" },
   { id: "operatingSystem", label: "سیستم عامل" },
-  { id: "ipv4", label: "IPv4" },
   { id: "statusId", label: "وضعیت" },
   { id: "createDate", label: "تاریخ ایجاد" },
   { id: "control", label: "", disableSort: true },
