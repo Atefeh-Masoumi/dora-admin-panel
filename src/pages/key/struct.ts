d4f33d8008ab5c1be02e28bf5dbfeea96945919b
atefeh <a.masoumi@dorsa.cloud>
2025-07-07 15:43:26 +0330

    feat(key): implement KeyListTableRow component for displaying and deleting VM keys with confirmation dialog

---
+++ src/pages/key/struct.ts
@@ -0,0 +1,9 @@
+import { columnType } from "src/components/organisms/tables/table.types";
+
+export const keyTableStruct: columnType[] = [
+  { label: "شناسه", id: "id" },
+  { label: "نام کلید", id: "name" },
+  { label: "Finger print", id: "fingerPrint" },
+  { label: "تاریخ ایجاد", id: "createDate" },
+  { label: "عملیات", id: "control" },
+]; 
