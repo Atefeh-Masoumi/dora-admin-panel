11f22a6047197732f9139c8d34931b76e357b2e7
atefeh <a.masoumi@dorsa.cloud>
2025-07-07 13:32:55 +0330

    feat(network): add networkTableStruct for defining the structure of the network table

---
+++ src/components/organisms/network/networklist/struct.ts
@@ -0,0 +1,11 @@
+import { columnType } from "../../tables/table.types";
+
+export const networkTableStruct: columnType[] = [
+  { label: "شناسه", id: "id" },
+  { label: "نام شبکه", id: "name" },
+  { label: "CIDR", id: "cidr" },
+  { label: "Subnet Mask", id: "subnetMask" },
+  { label: "Gateway IP", id: "gatewayIp" },
+  { label: "تاریخ ایجاد", id: "createDate" },
+  { label: "عملیات", id: "control" },
+];
