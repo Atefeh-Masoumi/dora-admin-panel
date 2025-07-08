b1a04d50c25624826cadecf2384afe8fe59146ad
atefeh <a.masoumi@dorsa.cloud>
2025-07-08 11:54:55 +0330

    refactor(access-keys): update AccessKeyStruct labels and remove secret key entry

--- src/components/organisms/storage/edit/tables/AccessKeyStruct.ts
+++ src/components/organisms/storage/edit/tables/AccessKeyStruct.ts
@@ -2,8 +2,8 @@ import { columnType } from "src/components/organisms/tables/table.types";
 
 export const accessKeyTableStruct: columnType[] = [
   { id: "id", label: "" },
-  { id: "accessKey", label: "کلید دسترسی" },
-  { id: "secretKey", label: "secret key" },
+  { id: "accessKey", label: "کلید " },
+  // { id: "secretKey", label: "secret key" },
   { id: "createDate", label: "تاریخ ایجاد" },
   { id: "control", label: "", disableSort: true },
 ];
