b2489685d222b3b358bd01e2c78cbca4c34238f8
atefeh <a.masoumi@dorsa.cloud>
2025-07-14 15:14:59 +0330

    fix(navigation): update navigate path in KubernetesNodesTableRow to use vmHostId for accurate routing

--- src/components/organisms/kuberCluster/edit/editNodes/table/struct.ts
+++ src/components/organisms/kuberCluster/edit/editNodes/table/struct.ts
@@ -2,7 +2,7 @@ import { columnType } from "src/components/organisms/tables/table.types";
 
 export const kubernetesNodesTableStruct: columnType[] = [
   { id: "hostId", label: "" },
-  { id: "ip", label: "ip" },
+  // { id: "ip", label: "ip" },
   { id: "name", label: "نام نود" },
   { id: "statusId", label: "وضعیت" },
   { id: "kuberClusterNodeType", label: "نوع نود" },
