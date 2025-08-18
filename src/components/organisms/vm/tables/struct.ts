93cdfae07ac3f92b5c1f483067d1493101eea014
Atefeh masoumi <a.masoumi@dorsa.cloud>
2025-08-18 14:05:14 +0330

    fix(vm): rename ipAddress to ipAddressString in table structure and update corresponding condition in VmTableRow component

--- src/components/organisms/vm/tables/struct.ts
+++ src/components/organisms/vm/tables/struct.ts
@@ -4,7 +4,7 @@ export const addVmTableStruct: columnType[] = [
   { id: "id", label: "", invisibility: true },
   { id: "name", label: "نام ماشین" },
   { id: "operatingSystem", label: "سیستم عامل" },
-  { id: "ipAddress", label: "IPv4" },
+  { id: "ipAddressString", label: "IPv4" },
   { id: "statusId", label: "وضعیت" },
   { id: "createDate", label: "تاریخ ایجاد" },
   { id: "control", label: "", disableSort: true },
