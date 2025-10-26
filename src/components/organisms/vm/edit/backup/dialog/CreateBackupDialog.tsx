eef0ddee265f5cee5a91bc8072504ea7e316d5a7
Atefeh masoumi <a.masoumi@dorsa.cloud>
2025-10-26 17:30:32 +0330

    feat(CreateBackupDialog): implement dialog for creating VM backups with form validation and API integration

---
+++ src/components/organisms/vm/edit/backup/dialog/CreateBackupDialog.tsx
@@ -0,0 +1,137 @@
+import { FC, MouseEventHandler } from "react";
+import {
+  DialogProps,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  Stack,
+  Button,
+  DialogActions,
+} from "@mui/material";
+import { useFormik } from "formik";
+import * as yup from "yup";
+import { formikOnSubmitType } from "src/types/form.type";
+import { usePostApiMyVmByProjectIdHostAndVmHostIdBackupCreateMutation } from "src/app/services/api.generated";
+import { toast } from "react-toastify";
+import { DorsaTextField } from "src/components/atoms/DorsaTextField";
+import LoadingButton from "src/components/atoms/LoadingButton";
+import { useParams } from "react-router";
+
+type CreateBackupDialogPropsType = DialogProps & {
+  forceClose: () => void;
+  refetch: () => void;
+};
+
+export const CreateBackupDialog: FC<CreateBackupDialogPropsType> = ({
+  forceClose,
+  refetch,
+  ...props
+}) => {
+  const [createBackup, { isLoading: createBackupLoading }] =
+  usePostApiMyVmByProjectIdHostAndVmHostIdBackupCreateMutation();
+
+  const initialValues = {
+    name: "",
+    description: "",
+  };
+
+  const { projectId, id:vmId } = useParams();
+  console.log(projectId, vmId);
+  const onSubmit: formikOnSubmitType<typeof initialValues> = (
+    { name, description },
+    { setSubmitting }
+  ) => {
+    
+    if (name.length < 5 || name.length > 50) {
+      toast.error("تعداد کارکترهای نام بکاپ باید حداقل 5 عدد باشد و حداکثر 50 عدد باشد");
+      return;
+    }
+
+    createBackup({
+      createVmHostBackupModel: {
+        name,
+        description,
+      },
+      projectId: Number(projectId),
+      vmHostId: Number(vmId),
+    })
+      .unwrap()
+      .then(() => {
+        toast.success("بکاپ   با موفقیت ایجاد شد");
+        forceClose();
+        refetch();
+      })
+      .catch((err) => {})
+      .finally(() => {
+        setSubmitting(false);
+      });
+  };
+
+  const formik = useFormik({
+    initialValues,
+    validationSchema: yup.object().shape({
+      name: yup
+        .string()
+        .min(2, "تعداد کارکترهای نام بکاپ باید حداقل 2 عدد باشد")
+        .max(50, "تعداد کارکترهای نام بکاپ باید حداقل ۵۰ عدد باشد")
+        .required("این بخش الزامی می‌باشد"),
+    }),
+    onSubmit,
+  });
+
+  const cancelBtnOnClick: MouseEventHandler<HTMLButtonElement> = (event) => {
+    if (!props.onClose) return;
+    props.onClose(event, "backdropClick");
+  };
+
+  return (
+    <Dialog {...props}>
+      <DialogTitle align="center">ایجاد بکاپ </DialogTitle>
+      <form onSubmit={formik.handleSubmit}>
+        <DialogContent>
+          <Stack rowGap={3} pt={2}>
+            <DorsaTextField
+              focused
+              {...formik.getFieldProps("name")}
+              error={Boolean(formik.errors.name && formik.touched.name)}
+              helperText={formik.errors.name}
+              label="نام"
+              inputProps={{ dir: "ltr" }}
+            />
+            <DorsaTextField
+              {...formik.getFieldProps("description")}
+              error={Boolean(
+                formik.errors.description && formik.touched.description
+              )}
+              helperText={formik.errors.description}
+              multiline
+              minRows={3}
+              maxRows={8}
+              label="توضیحات"
+            />
+          </Stack>
+        </DialogContent>
+        <DialogActions>
+          <Stack direction="row" justifyContent="end" spacing={1}>
+            <Button
+              variant="outlined"
+              color="secondary"
+              sx={{ px: 3, py: 0.8 }}
+              onClick={cancelBtnOnClick}
+            >
+              انصراف
+            </Button>
+            <LoadingButton
+              type="submit"
+              loading={createBackupLoading}
+              variant="contained"
+              sx={{ px: 3, py: 0.8 }}
+            >
+              ذخیره
+            </LoadingButton>
+          </Stack>
+        </DialogActions>
+      </form>
+    </Dialog>
+  );
+}; 
