c01f2e5b5ca8b9ab70716b63fabe2d1ab4e9cc75
Atefeh masoumi <a.masoumi@dorsa.cloud>
2025-10-27 13:10:22 +0330

    feat(SelectEditEnvironmentVariable): create component for selecting and editing environment variables in deployment settings #150

---
+++ src/components/organisms/kubernetesCloud/edit/deployment/edit/edit/SelectEditEnvironmentVariable.tsx
@@ -0,0 +1,165 @@
+import { FC, memo, useMemo, useState } from "react";
+import { Grid, useMediaQuery, useTheme } from "@mui/material";
+import { FormikProps } from "formik";
+import { ENVIRONMENT_TYPES } from "src/constant/kubernetesCloud.constant";
+
+import { SelectEnvType } from "../../add/envVariable/SelectEnvType";
+import { SelectEnvKey } from "../../add/envVariable/SelectEnvKey";
+// import { SelectEnvValue } from "../../../add/envVariable/SelectEnvValue";
+import {
+  useGetApiMyKubernetesCloudByProjectIdHostAndKuberHostIdConfigmapListQuery,
+  useGetApiMyKubernetesCloudByProjectIdHostAndKuberHostIdSecretListQuery,
+} from "src/app/services/api.generated";
+import { useParams } from "react-router-dom";
+import { getResourceItems } from "src/utils/getResourceItems.utils";
+import { KeyListInResourceType } from "src/types/kubernetesCloud.types";
+import { SelectEnvValue } from "../../add/envVariable/SelectEnvValue";
+
+type EditDeploymentFormType = {
+  replicaNumber: number;
+  keyValue: {
+    variableType: number;
+    envKey: string;
+    value: string;
+    id?: number;
+  }[];
+  envToDelete: number[];
+};
+
+type SelectEditEnvironmentVariablePropsType = {
+  mainIndex: number;
+  onDelete: () => void;
+  formik: FormikProps<EditDeploymentFormType>;
+};
+
+export const SelectEditEnvironmentVariable: FC<SelectEditEnvironmentVariablePropsType> =
+  memo(({ formik, onDelete, mainIndex }) => {
+    const [keyListInResource, setKeyListInResource] =
+      useState<KeyListInResourceType>();
+    const [selectedResourceItem, setSelectedResourceItem] = useState<
+      number | null
+    >(null);
+    const theme = useTheme();
+    const isSm = useMediaQuery(theme.breakpoints.up("sm"));
+    const { kubernetesCloudId, projectId } = useParams();
+    const namespaceId = Number(kubernetesCloudId);
+
+    const { data: configmapList = [] } =
+      useGetApiMyKubernetesCloudByProjectIdHostAndKuberHostIdConfigmapListQuery(
+        { kuberHostId: Number(kubernetesCloudId), projectId: Number(projectId) },
+        { skip: !namespaceId }
+      );
+
+    const { data: secretList = [] } =
+      useGetApiMyKubernetesCloudByProjectIdHostAndKuberHostIdSecretListQuery(
+        { kuberHostId: Number(kubernetesCloudId), projectId: Number(projectId) },
+        { skip: !namespaceId }
+      );
+
+    const isResourceSelectionRequired = useMemo(
+      () => {
+        const keyValueItem = formik.values.keyValue[mainIndex];
+        return keyValueItem
+          ? keyValueItem.variableType !== ENVIRONMENT_TYPES.CUSTOM
+          : false;
+      },
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+      [formik.values.keyValue[mainIndex]]
+    );
+
+    const resourceList = useMemo(() => {
+      if (!formik.values.keyValue[mainIndex]) return [];
+      const variableType = formik.values.keyValue[mainIndex].variableType;
+      return isResourceSelectionRequired
+        ? variableType === ENVIRONMENT_TYPES.CONFIG_MAP
+          ? configmapList
+          : secretList
+        : [];
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [
+      isResourceSelectionRequired,
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+      formik.values.keyValue[mainIndex],
+      configmapList,
+      secretList,
+    ]);
+
+    const handleResourceOnChange = (resourceId: number) => {
+      const resourceType = formik.values.keyValue[mainIndex].variableType;
+      const resourceItems = getResourceItems(
+        resourceType,
+        resourceId,
+        configmapList,
+        secretList
+      );
+
+      if (resourceItems) {
+        setKeyListInResource(resourceItems);
+      }
+
+      setSelectedResourceItem(resourceId);
+    };
+
+    const inputItems = [
+      {
+        Input: SelectEnvType,
+        type: "input",
+        xs: 12,
+        sm: 2.5,
+        md: 2.5,
+        lg: 2.5,
+        value: formik.values.keyValue[mainIndex]?.variableType || "",
+        onChange: (newValue: string | number) =>
+          formik.setFieldValue(`keyValue[${mainIndex}].variableType`, newValue),
+      },
+      {
+        Input: SelectEnvKey,
+        type: "input",
+        xs: 12,
+        sm: 2.5,
+        md: 2.5,
+        lg: 2.5,
+        value: formik.values.keyValue[mainIndex]?.envKey || "",
+        onChange: (newValue: string | number) =>
+          formik.setFieldValue(`keyValue[${mainIndex}].envKey`, newValue),
+      },
+      {
+        Input: SelectEnvValue,
+        type: "input",
+        xs: 12,
+        sm: 7,
+        md: 7,
+        lg: 7,
+        value: formik.values.keyValue[mainIndex]?.value || "",
+        onChange: (newValue: string | number) =>
+          formik.setFieldValue(`keyValue[${mainIndex}].value`, newValue),
+        otherProps: {
+          keyListInResource: keyListInResource || [],
+          isResourceSelectionRequired: isResourceSelectionRequired || false,
+          selectedResourceItem: selectedResourceItem,
+          resourceList: resourceList || [],
+          handleResourceOnChange,
+          onDelete: onDelete,
+        },
+      },
+    ];
+
+    return (
+      <Grid spacing={2} container>
+        {[...(!isSm ? inputItems.reverse() : inputItems)]
+          .reverse()
+          .map(
+            ({ Input, value, onChange, otherProps, xs, sm, md, lg }, index) => (
+              <Grid key={index} item xs={xs} sm={sm} md={md} lg={lg}>
+                <Input
+                  value={value}
+                  onChange={onChange}
+                  {...(otherProps || undefined)}
+                />
+              </Grid>
+            )
+          )}
+      </Grid>
+    );
+  });
+
