abdbcbc45185e6a2a0d5e6b44c412a2e21d6a317
Atefeh masoumi <a.masoumi@dorsa.cloud>
2025-10-27 09:58:56 +0330

    fix(Router): update link text and URL for Kubernetes Cloud firewall management

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -869,7 +869,11 @@ const Router: FC = () => {
         <Route
           path="/kubernetes-cloud/:projectId/:kubernetesCloudId/firewall"
           element={mainTemplate(EditKubernetesCloud, {
-            pageTitle: "مدیریت Firewall",
+            link: {
+              text: "بازگشت به مدیریت کوبرنتیز ابری",
+              url: "/kubernetes-cloud/:projectId",
+            },
+            hideSidebar: false,
           })}
         />
 
