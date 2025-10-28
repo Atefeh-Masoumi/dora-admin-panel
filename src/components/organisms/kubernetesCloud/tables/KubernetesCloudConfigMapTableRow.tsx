b409845c229a8e7562f480317f4bbde51aa08434
Atefeh masoumi <a.masoumi@dorsa.cloud>
2025-10-28 09:10:03 +0330

    refactor(KubernetesCloudSecretMapTableRow): comment out 'id' column in secret map table structure for future consideration

--- src/components/organisms/kubernetesCloud/tables/KubernetesCloudConfigMapTableRow.tsx
+++ src/components/organisms/kubernetesCloud/tables/KubernetesCloudConfigMapTableRow.tsx
@@ -177,9 +177,9 @@ export const KubernetesCloudConfigMapTableRow: FC<{
                     ? configList?.map((item: any, index: any) => {
                         return (
                           <TableRow key={index}>
-                            <TableCell align="center" sx={{ border: "none" }}>
+                            {/* <TableCell align="center" sx={{ border: "none" }}>
                               {item.id}
-                            </TableCell>
+                            </TableCell> */}
                             <TableCell align="center" sx={{ border: "none" }}>
                               {item.key}
                             </TableCell>
