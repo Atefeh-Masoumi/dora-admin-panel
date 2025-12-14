e42b7ed8db599726aafb606aaabf0605e8a08d8a
Atefeh masoumi <a.masoumi@dorsa.cloud>
2025-12-14 18:18:00 +0330

    feat(IPManagement): add EnablePortDialog and DisablePortDialog components for managing port security; enhance IPRow with security toggle functionality and update VmIpAddress to include network display

--- src/components/organisms/vm/edit/ip/IPRow.tsx
+++ src/components/organisms/vm/edit/ip/IPRow.tsx
@@ -2,15 +2,20 @@ import { useDeleteApiMyVmByProjectIdHostAndVmHostIdIpDeleteIdMutation } from "sr
 import {
   CheckCircleOutline as CheckCircleOutlineIcon,
   Cancel as CancelIcon,
+  LockOutlined,
+  LockOpenOutlined,
 } from "@mui/icons-material";
 import { toast } from "react-toastify";
 import { FC, useState } from "react";
 import { DeleteSvg } from "src/components/atoms/svg-icons/DeleteSvg";
 import { BORDER_RADIUS_1 } from "src/configs/theme";
-import { Grid2 } from "@mui/material";
+import { Grid2, Tooltip } from "@mui/material";
 import { CircularProgress, IconButton, Typography } from "@mui/material";
 import { IPDeleteDialog } from "./dialog/IPDeleteDialog";
 import { useParams } from "react-router";
+import { LockUnlockSvg } from "src/components/atoms/svg-icons/LockUnlockSvg";
+import { EnablePortDialog } from "./dialog/EnablePortDialog";
+import { DisablePortDialog } from "./dialog/disablePortDialog";
 
 type IpRowProps = {
   refetch: () => void;
@@ -18,12 +23,14 @@ type IpRowProps = {
   isPrimary?: boolean;
   vmHostId: number;
   id: number;
+  isEnableSecurity: boolean;
 }
 
-const IpRow: FC<IpRowProps> = ({ ipAddress, isPrimary, vmHostId, id, refetch }) => {
+const IpRow: FC<IpRowProps> = ({ ipAddress, isPrimary, vmHostId, id, isEnableSecurity, refetch }) => {
   const [deleteIp, { isLoading }] = useDeleteApiMyVmByProjectIdHostAndVmHostIdIpDeleteIdMutation();
   const [isDeleteDialogOpen, setIsDeleteDialogOpen] = useState(false); // State to control the dialog
-
+  const [isEnablePortDialogOpen, setIsEnablePortDialogOpen] = useState(false);
+  const [isDisableDialogOpen, setIsDisableDialogOpen] = useState(false);
   const { projectId } = useParams();
   const handleDelete = () => {
     if (id) {
@@ -49,6 +56,7 @@ const IpRow: FC<IpRowProps> = ({ ipAddress, isPrimary, vmHostId, id, refetch }) 
   const closeDeleteDialog = () => {
     setIsDeleteDialogOpen(false);
   };
+
 
   return (
     <>
@@ -88,9 +96,27 @@ const IpRow: FC<IpRowProps> = ({ ipAddress, isPrimary, vmHostId, id, refetch }) 
               <CircularProgress size={25} />
             </IconButton>
           ) : (
-            <IconButton onClick={openDeleteDialog}>
-              <DeleteSvg />
-            </IconButton>
+            <>
+              <IconButton onClick={openDeleteDialog}>
+                <DeleteSvg />
+              </IconButton>
+              {
+                isEnableSecurity ? <>
+                  <Tooltip title="فعال سازی غیرport security">
+                    <IconButton onClick={()=> setIsDisableDialogOpen(true) } sx={{ cursor: "pointer" }}>
+                      <LockOpenOutlined sx={{ color: "grey.700" }} />
+                    </IconButton>
+                  </Tooltip>
+                </>
+                  :
+
+                  <Tooltip title="فعال سازی port security">
+                    <IconButton onClick={() => setIsEnablePortDialogOpen(true)} sx={{ cursor: "pointer" }}>
+                      <LockOutlined sx={{ color: "grey.700" }} />
+                    </IconButton>
+                  </Tooltip>
+              }
+            </>
           )}
         </Grid2>
       </Grid2>
@@ -102,6 +128,17 @@ const IpRow: FC<IpRowProps> = ({ ipAddress, isPrimary, vmHostId, id, refetch }) 
         title="حذف IP"
         message="آیا از حذف این IP اطمینان دارید؟ این عمل غیرقابل بازگشت است."
       />
+      <EnablePortDialog
+        openDialog={isEnablePortDialogOpen}
+        handleClose={() => setIsEnablePortDialogOpen(false)}
+        id={id}
+        refetch={refetch}
+      />
+      <DisablePortDialog
+       openDialog={isDisableDialogOpen}
+       handleClose={() => setIsDisableDialogOpen(false)}
+       id={id}
+       refetch={refetch}/>
     </>
   );
 };
