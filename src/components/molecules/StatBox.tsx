6008500e4ec1c263b74e782ff6ee8ce6bf54d67f
Atefeh masoumi <a.masoumi@dorsa.cloud>
2025-12-16 18:06:26 +0330

    refactor(StatBox): enhance layout and typography for improved readability, adjust padding and border radius, and streamline component structure

--- src/components/molecules/StatBox.tsx
+++ src/components/molecules/StatBox.tsx
@@ -1,42 +1,56 @@
-import { Box, Grid, Paper, Stack, Typography } from "@mui/material";
+import { Paper, Stack, Typography, Box } from "@mui/material";
 
 type StatBoxProps = {
   title: string;
   value: string | number;
   unit?: string;
-  color?: string;
   img?: string;
+  color?:string;
 };
 
-export const StatBox = ({ title, value, unit, color, img }: StatBoxProps) => {
+export const StatBox = ({ title, value, unit, img }: StatBoxProps) => {
   return (
     <Paper
       elevation={0}
       sx={{
-        p: 2,
-        borderRadius: 3,
+        p: 1.5,
+        borderRadius: 2,
         border: "1px solid rgba(0,0,0,0.08)",
-        flex: 1,
         direction: "rtl",
+        flex: 1,
+        minWidth: 180,
       }}
     >
-      <Stack spacing={1}>
-        <Typography color="grey.700" fontSize={24} fontWeight={500}>
+      <Stack spacing={0.5}>
+        <Typography fontSize={14} fontWeight={500} color="grey.700">
           {title}
         </Typography>
-        <Stack direction="row" alignItems="center" justifyContent="space-between">
 
-        <Typography fontSize={28} fontWeight={700} color={ "black"}>
-          {value}
-          <Typography fontSize={18} fontWeight={500} sx={{ display: "inline-block", padding:3}} color="black">
-            {unit}
-          </Typography>
-        </Typography>
-        <img
+        <Stack direction="row" alignItems="center" justifyContent="space-between">
+          <Box display={"flex"}>
+            <Typography fontSize={20} fontWeight={700} color="black">
+              {value}
+            </Typography>
+              {unit && (
+                <Typography
+                  component="span"
+                  fontSize={12}
+                  fontWeight={500}
+                  sx={{ m: 1.5 }}
+                >
+                  {unit}
+                </Typography>
+              )}
+          </Box>
+
+          {img && (
+            <Box
+              component="img"
               src={img}
-              alt="stat box"
-              style={{ width: "120px", height: "80px", margin: "0" }}
+              alt="stat"
+              sx={{ width: 40, height: 40 }}
             />
+          )}
         </Stack>
       </Stack>
     </Paper>
