87e57118d0f036bcf1550f9360d60eb81abd312c
Atefeh masoumi <a.masoumi@dorsa.cloud>
2025-12-16 18:08:22 +0330

    feat(KeyTableStruct): add status column to key table structure for enhanced key management

--- src/pages/key/struct.ts
+++ src/pages/key/struct.ts
@@ -4,6 +4,7 @@ export const keyTableStruct: columnType[] = [
   { label: "شناسه", id: "id" },
   { label: "نام کلید", id: "name" },
   { label: "Finger print", id: "fingerPrint" },
+  { label: " وضعیت", id: "statusId" },
   { label: "تاریخ ایجاد", id: "createDate" },
   { label: "", id: "control" },
 ]; 
