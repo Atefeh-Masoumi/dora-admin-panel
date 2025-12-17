ebaac60148e9bf88a558acf9f5daa7b616326fe6
Atefeh masoumi <a.masoumi@dorsa.cloud>
2025-12-17 15:14:36 +0330

    refactor(SelectNetwork): enhance network and firewall settings UI with new components and improved functionality

--- src/components/organisms/vm/add/steps/SelectNetwork.tsx
+++ src/components/organisms/vm/add/steps/SelectNetwork.tsx
@@ -1,8 +1,27 @@
-import { FC, useContext, useMemo } from "react";
-import { Box, Checkbox, Divider, FormControl, FormControlLabel, InputLabel, MenuItem, Paper, Select, Stack, TextField, Typography } from "@mui/material";
-import { AddServerContext } from "src/components/organisms/vm/add/contexts/AddVmContext";
+import { FC, useContext, useState } from "react";
+import {
+  Box,
+  Button,
+  Checkbox,
+  FormControl,
+  FormControlLabel,
+  IconButton,
+  InputLabel,
+  MenuItem,
+  Paper,
+  Select,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
 import { useParams } from "react-router-dom";
-import { useGetApiMyVmByProjectIdNetworkShortListQuery } from "src/app/services/api.generated";
+import {
+  useGetApiMyVmByProjectIdFirewallListQuery,
+  useGetApiMyVmByProjectIdNetworkShortListQuery,
+} from "src/app/services/api.generated";
+import { AddServerContext } from "src/components/organisms/vm/add/contexts/AddVmContext";
+import { AddFirewallDialog } from "src/pages/firewall/AddFirewall";
+import RefreshIcon from "@mui/icons-material/Refresh";
 
 export const SelectNetwork: FC = () => {
   const { projectId } = useParams();
@@ -17,11 +36,23 @@ export const SelectNetwork: FC = () => {
     setSelectedNetwork,
     ipAddress,
     setIpAddress,
+    usedFirewall,
+    setUsedFirewall,
+    vmFirewallId,
+    setVmFirewallId,
   } = useContext(AddServerContext);
 
-  const { data: vmNetworkList } = useGetApiMyVmByProjectIdNetworkShortListQuery({
-    projectId: Number(projectId),
-  });
+  const { data: vmNetworkList } = useGetApiMyVmByProjectIdNetworkShortListQuery(
+    {
+      projectId: Number(projectId),
+    }
+  );
+  const { data: firewallList = [], refetch: refetchFirewallList } =
+    useGetApiMyVmByProjectIdFirewallListQuery({
+      projectId: Number(projectId),
+    });
+
+  const [isAddFirewallOpen, setIsAddFirewallOpen] = useState(false);
 
   const handlePrivateNetworkChange = (
     event: React.ChangeEvent<HTMLInputElement>
@@ -33,48 +64,89 @@ export const SelectNetwork: FC = () => {
     }
   };
 
-  return (
-    <Paper sx={{ p: 2,boxShadow: "none" }}>
-      <Stack direction="column" rowGap={2}>
-      <Typography fontSize={24} fontWeight="bold" align="center">
+  const handleFirewallChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setUsedFirewall(event.target.checked);
+    if (!event.target.checked) {
+      setVmFirewallId(null);
+    }
+  };
 
-        تنظیمات شبکه</Typography>
-        {/* <Divider flexItem sx={{ borderWidth: 1.5 }} /> */}
+  return (
+    <Paper sx={{ p: 2, boxShadow: "none" }}>
+      <Stack direction="column" rowGap={2}>
+        <Typography fontSize={24} fontWeight="bold" align="center">
+          تنظیمات شبکه
+        </Typography>
         <Typography
           align="center"
           fontSize={16}
           sx={{ color: ({ palette }) => palette.grey[700] }}
         >
-          از این قسمت می توانید تنظیمات شبکه خود را تنظیم کنید
+          تنظیمات اتصال شبکه و فایروال مورد نیاز سرور را انتخاب کنید
         </Typography>
         <Stack gap={2} direction={{ md: "column", xl: "column" }}>
-          <Box p={1.5} sx={{ width: "100%", border: ({ palette }) => `1px solid ${palette.grey}` }}>
+          <Box
+            p={1.5}
+            sx={{
+              width: "100%",
+              border: ({ palette }) => `1px solid ${palette.grey}`,
+            }}
+          >
             <FormControlLabel
-              control={<Checkbox checked={usePublicIpV4} onChange={(e) => setUsePublicIpV4(e.target.checked)} />}
-              label="استفاده از آدرس IP عمومی IPv4"
+              control={
+                <Checkbox
+                  checked={usePublicIpV4}
+                  onChange={(e) => setUsePublicIpV4(e.target.checked)}
+                />
+              }
+              label="استفاده از IP عمومی IPv4"
             />
-            <Typography  pt={1} pr={1} pl={4}>
-              با استفاده از آدرس IP عمومی IPv4 به سرور خود دسترسی داشته باشید
+            <Typography pt={1} pr={1} pl={4}>
+              با فعال‌سازی IP عمومی نسخه IPv4، سرور از اینترنت قابل دسترسی خواهد
+              بود.
             </Typography>
           </Box>
 
-          <Box p={1.5} sx={{ width: "100%", border: ({ palette }) => `1px solid ${palette.grey}` }}>
+          <Box
+            p={1.5}
+            sx={{
+              width: "100%",
+              border: ({ palette }) => `1px solid ${palette.grey}`,
+            }}
+          >
             <FormControlLabel
-              control={<Checkbox checked={usePublicIpV6} onChange={(e) => setUsePublicIpV6(e.target.checked)} />}
-              label="استفاده از آدرس IP عمومی IPv6"
+              control={
+                <Checkbox
+                  checked={usePublicIpV6}
+                  onChange={(e) => setUsePublicIpV6(e.target.checked)}
+                />
+              }
+              label="استفاده از IP عمومی IPv6"
             />
-            <Typography  pt={1} pr={1} pl={4}>
-              با استفاده از آدرس IP عمومی IPv6 به سرور خود دسترسی داشته باشید
+            <Typography pt={1} pr={1} pl={4}>
+              با فعال‌سازی IP عمومی نسخه IPv6، سرور از اینترنت قابل دسترسی خواهد
+              بود.
             </Typography>
           </Box>
 
-          <Box p={1.5} sx={{ width: "100%", border: ({ palette }) => `1px solid ${palette.grey}` }}>
+          <Box
+            p={1.5}
+            sx={{
+              width: "100%",
+              border: ({ palette }) => `1px solid ${palette.grey}`,
+            }}
+          >
             <FormControlLabel
-              control={<Checkbox checked={usePrivateNetwork} onChange={handlePrivateNetworkChange} />}
+              control={
+                <Checkbox
+                  checked={usePrivateNetwork}
+                  onChange={handlePrivateNetworkChange}
+                />
+              }
               label="استفاده از شبکه خصوصی"
             />
-            <Typography  pt={1} pr={1} pl={4}>
-              با استفاده از شبکه خصوصی، سرور خود را به شبکه‌های دیگر متصل کنید
+            <Typography pt={1} pr={1} pl={4}>
+              با استفاده از شبکه خصوصی، سرور خود را به شبکه‌های دیگر متصل کنید.
             </Typography>
 
             {usePrivateNetwork && (
@@ -86,7 +158,10 @@ export const SelectNetwork: FC = () => {
                     label="انتخاب شبکه"
                     onChange={(e) => {
                       const selectedId = Number(e.target.value);
-                      const network = vmNetworkList?.find((n) => Number(n.id) === selectedId) || null;
+                      const network =
+                        vmNetworkList?.find(
+                          (n) => Number(n.id) === selectedId
+                        ) || null;
                       setSelectedNetwork(network);
                     }}
                   >
@@ -108,6 +183,81 @@ export const SelectNetwork: FC = () => {
               </Stack>
             )}
           </Box>
+
+          <Box
+            p={1.5}
+            sx={{
+              width: "100%",
+              border: ({ palette }) => `1px solid ${palette.grey}`,
+            }}
+          >
+            <Stack direction="row" alignItems="center" gap={1}>
+              <Stack flex="column" flexGrow={1}>
+                <FormControlLabel
+                  control={
+                    <Checkbox
+                      checked={usedFirewall}
+                      onChange={handleFirewallChange}
+                    />
+                  }
+                  label="استفاده از فایروال"
+                />
+                <Typography pt={1} pr={1} pl={4}>
+                  با فعال‌سازی فایروال، ترافیک ورودی و خروجی سرور کنترل می‌شود.
+                </Typography>
+              </Stack>
+              {usedFirewall && (
+              <Button
+                variant="outlined"
+                color="secondary"
+                onClick={() => setIsAddFirewallOpen(true)}
+                sx={{ minWidth: 180 }}
+              >
+                افزودن فایروال جدید
+              </Button>)}
+            </Stack>
+            {usedFirewall && (
+              <Stack direction="column" gap={2} mt={2}>
+                <Stack direction={{ xs: "column", sm: "row" }} gap={1}>
+                  {/* <IconButton
+                    onClick={() => refetchFirewallList()}
+                    sx={{ minWidth: 180 }}
+                  >
+                    <RefreshIcon
+                      style={{
+                        transform: `rotate(360deg)`,
+                        transition: "transform 0.7s ease",
+                      }}
+                    />
+                  </IconButton> */}
+                </Stack>
+
+                <FormControl fullWidth>
+                  <InputLabel>انتخاب فایروال</InputLabel>
+                  <Select
+                    value={vmFirewallId ?? ""}
+                    label="انتخاب فایروال"
+                    onChange={(e) => {
+                      setVmFirewallId(Number(e.target.value));
+                    }}
+                  >
+                    {firewallList?.map((firewall) => (
+                      <MenuItem key={firewall.id} value={Number(firewall.id)}>
+                        {firewall.name}
+                      </MenuItem>
+                    ))}
+                  </Select>
+                </FormControl>
+              </Stack>
+            )}
+          </Box>
+
+          <AddFirewallDialog
+            open={isAddFirewallOpen}
+            forceClose={() => setIsAddFirewallOpen(false)}
+            refetch={refetchFirewallList}
+            onClose={() => setIsAddFirewallOpen(false)}
+          />
         </Stack>
       </Stack>
     </Paper>
