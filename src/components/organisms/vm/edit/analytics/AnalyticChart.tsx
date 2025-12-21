148680106f98228522c9eee1f8dfe179f7c9abfb
Atefeh masoumi <a.masoumi@dorsa.cloud>
2025-12-21 12:33:57 +0330

    fix(AnalyticChart): calculate total upload and download values for accurate display in the statistics box

--- src/components/organisms/vm/edit/analytics/AnalyticChart.tsx
+++ src/components/organisms/vm/edit/analytics/AnalyticChart.tsx
@@ -61,7 +61,9 @@ export const AnalyticChart: FC<AnalyticChartPropsType> = () => {
     [getDataFetching, getDataLoading]
   );
   const formatStatValue = (value?: number) => typeof value === "number" ? value.toFixed(4) : "0";
-
+  const total =
+  (userAnalytics?.totalUpload ?? 0) +
+  (userAnalytics?.totalDownload ?? 0);
   
 
   return (
@@ -122,7 +124,7 @@ export const AnalyticChart: FC<AnalyticChartPropsType> = () => {
 
           <StatBox
             title="Total"
-            value={(formatStatValue(userAnalytics?.totalUpload) ?? 0) + (formatStatValue(userAnalytics?.totalDownload) ?? 0)}
+            value={(formatStatValue(total)) ?? 0 }
             unit="GB"
             img={UploadImage}
           />
