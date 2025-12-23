bc95cfa143551fdac4978f38a6f47c7fc40120f8
Atefeh masoumi <a.masoumi@dorsa.cloud>
2025-12-23 15:30:26 +0330

    fix(AnalyticChart): update initial categoryId state and periodId calculation for accurate analytics data

--- src/components/organisms/vm/edit/analytics/AnalyticChart.tsx
+++ src/components/organisms/vm/edit/analytics/AnalyticChart.tsx
@@ -39,7 +39,7 @@ export const analyticsCategories = [
 //   const { id, projectId } = useParams();
 //   const vmHostId = Number(id) || 0;
 
-//   const [categoryId, setCategoryId] = useState(0);
+  const [categoryId, setCategoryId] = useState(1);
 
   const handleChange = (event: SelectChangeEvent) => {
     setCategoryId(+event.target.value);
@@ -51,7 +51,7 @@ export const analyticsCategories = [
   } = useGetApiMyVmByProjectIdHostGetAnalyticAndIdQuery({
     id: vmHostId,
     projectId: Number(projectId),
-    periodId: categoryId + 1,
+    periodId: categoryId ,
   }, {
     skip: !projectId || !vmHostId,
   });
