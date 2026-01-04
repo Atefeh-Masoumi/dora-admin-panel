82d442282b92b86d5062a235107095d3d989f5fc
Atefeh masoumi <a.masoumi@dorsa.cloud>
2026-01-04 15:34:44 +0330

    fix(VolumeTableRow): update API mutation for volume deletion to reflect new endpoint

--- src/components/organisms/vm/edit/volume/table/VolumeTableRow.tsx
+++ src/components/organisms/vm/edit/volume/table/VolumeTableRow.tsx
@@ -8,7 +8,7 @@ import { withTableRowWrapper } from "src/HOC/withTableRowWrapper";
 import { volumeTableStruct } from "./struct";
 import {
   VolumeListResponse,
-  useDeleteApiMyVmByProjectIdHostAndVmHostIdIpDeleteIdMutation,
+  useDeleteApiMyVmByProjectIdVolumeDeleteAndIdMutation,
 } from "src/app/services/api.generated";
 import PageLoading from "src/components/atoms/PageLoading";
 import { useParams } from "react-router";
@@ -37,7 +37,7 @@ export const VolumeTableRow: FC<VolumeTableRowProps> = ({
   const [selectedVm, setSelectedVm] = useState<VolumeListResponse | null>(null);
   const { id, projectId } = useParams();
   const [deleteItem, { isLoading: deleteVmRecordLoading }] =
-    useDeleteApiMyVmByProjectIdHostAndVmHostIdIpDeleteIdMutation();
+    useDeleteApiMyVmByProjectIdVolumeDeleteAndIdMutation();
 
   const onEnableAutoBackupBtnClick = () => {
     onEnableAutoBackupClick(row);
@@ -58,17 +58,17 @@ export const VolumeTableRow: FC<VolumeTableRowProps> = ({
   };
 
   const deleteVmRecordHandler = () =>
-    deleteItem({
-      id: Number(selectedVm?.id),
-      projectId: Number(projectId),
-      vmHostId: Number(id),
-    })
+    deleteItem(
+      {
+        id: Number(selectedVm?.id),
+        projectId: Number(projectId),
+      })
       .unwrap()
       .then(() => {
         toast.success("دیسک با موفقیت حذف شد");
         closeDialogHandler();
       })
-      .catch((err) => {});
+      .catch(() => { });
 
   const getStatusConfig = (enabled: boolean) => {
     if (enabled) {
