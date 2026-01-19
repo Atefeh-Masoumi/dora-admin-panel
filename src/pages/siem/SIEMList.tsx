3a1fbfc1b8c80ebe683ef60985c1bc426c1feb7b
Atefeh masoumi <a.masoumi@dorsa.cloud>
2026-01-19 17:24:05 +0330

    feat: siemllist page

---
+++ src/pages/siem/SIEMList.tsx
@@ -0,0 +1,154 @@
+import { Add } from "@mui/icons-material";
+import { Box, Button, Divider, Stack, Typography } from "@mui/material";
+import { FC, useEffect, useState, useMemo } from "react";
+import { useParams } from "react-router-dom";
+import { SearchBox } from "src/components/molecules/SearchBox";
+import { BaseTable } from "src/components/organisms/tables/BaseTable";
+import { BORDER_RADIUS_1 } from "src/configs/theme";
+import { RefreshButton } from "src/components/atoms/RefreshButton";
+import {
+  useGetApiMySecurityByProjectIdSiemHostListQuery,
+  SiemListResponse,
+} from "src/app/services/api.generated";
+import { siemTableStruct } from "src/components/organisms/siem/siemList/struct";
+import SiemTableRow from "src/components/organisms/siem/siemList/SiemTableRow";
+
+
+enum DIALOG_TYPE_ENUM {
+  CREATE = "CREATE",
+  DELETE = "DELETE",
+}
+
+const SIEMList: FC = () => {
+  const { projectId } = useParams();
+  const [search, setSearch] = useState("");
+  const [dialogType, setDialogType] = useState<DIALOG_TYPE_ENUM | null>(null);
+  const [selectedSiem, setSelectedSiem] = useState<SiemListResponse | null>(null);
+
+  const {
+    data: SiemList = [],
+    isLoading: getsiemListLoading,
+    refetch,
+    isFetching,
+  } = useGetApiMySecurityByProjectIdSiemHostListQuery({
+    projectId: Number(projectId),
+  });
+
+  useEffect(() => {
+    const getNotifInterval = setInterval(() => {
+      refetch();
+    }, 120 * 1000);
+    return () => {
+      clearInterval(getNotifInterval);
+    };
+  }, [refetch]);
+
+  const filteredList = useMemo(() => {
+    if (SiemList.length === 0) return SiemList;
+    return (
+      SiemList.filter((item) =>
+        item.name?.trim().toLowerCase()?.includes(search.trim().toLowerCase())
+      ) || []
+    );
+  }, [search, SiemList]);
+
+  const createSiemOnClick = () => {
+    setDialogType(DIALOG_TYPE_ENUM.CREATE);
+  };
+
+  const closeDialogHandler = () => {
+    setDialogType(null);
+    setSelectedSiem(null);
+  };
+
+  return (
+    <>
+      <Stack
+        bgcolor="white"
+        py={3}
+        px={3}
+        width="100%"
+        borderRadius={BORDER_RADIUS_1}
+        direction="column"
+      >
+        <Stack
+          direction={{ xs: "column", md: "row" }}
+          justifyContent="space-between"
+          alignItems="center"
+          rowGap={3}
+        >
+          <Stack
+            direction={{ xs: "column", sm: "row" }}
+            alignItems="center"
+            spacing={2}
+          >
+            <Typography fontSize={18} color="secondary">
+              لیست SIEMها
+            </Typography>
+            <RefreshButton isFetching={isFetching} refetchData={refetch} />
+          </Stack>
+          <Stack
+            direction={{ xs: "column", sm: "row" }}
+            alignItems="center"
+            spacing={2}
+          >
+            <SearchBox
+              onChange={(text) => setSearch(text)}
+              placeholder="جستجو در نام  SIEM"
+            />
+            <Button
+              onClick={createSiemOnClick}
+              variant="outlined"
+              size="large"
+              sx={{
+                whiteSpace: "nowrap",
+                px: 1.2,
+                borderRadius: BORDER_RADIUS_1,
+              }}
+              startIcon={
+                <Stack
+                  alignItems="center"
+                  justifyContent="center"
+                  sx={{
+                    width: 24,
+                    height: 24,
+                    border: ({ palette }) =>
+                      "1px solid " + palette.primary.main,
+                    borderRadius: BORDER_RADIUS_1,
+                  }}
+                >
+                  <Add
+                    fontSize="small"
+                    sx={{ "& path": { stroke: "rgba(60, 138, 255, 1)" } }}
+                  />
+                </Stack>
+              }
+            >
+              ایجاد SIEM جدید
+            </Button>
+          </Stack>
+        </Stack>
+        <Divider sx={{ width: "100%", color: "#6E768A14", py: 1 }} />
+        <Box width="100%" sx={{ pt: 1.5 }}>
+          <BaseTable
+            struct={siemTableStruct}
+            RowComponent={SiemTableRow}
+            rows={filteredList}
+            text="در حال حاضر  SIEM وجود ندارد"
+            isLoading={getsiemListLoading}
+            initialOrder={0}
+          />
+        </Box>
+      </Stack>
+      {/* <AddPamDialog
+        open={dialogType === DIALOG_TYPE_ENUM.CREATE}
+        onClose={closeDialogHandler}
+        forceClose={closeDialogHandler}
+        refetch={refetch}
+      /> */}
+    </>
+  );
+};
+
+export default SIEMList;
+
