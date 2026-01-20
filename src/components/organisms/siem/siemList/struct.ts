1bcee10b7d742f6aaa1379a918ef2c28314abade
Atefeh masoumi <a.masoumi@dorsa.cloud>
2026-01-20 10:25:53 +0330

    feat: create siem table structure

---
+++ src/components/organisms/siem/siemList/struct.ts
@@ -0,0 +1,10 @@
+import { columnType } from "../../tables/table.types";
+
+export const siemTableStruct: columnType[] = [
+  { label: "نام  ", id: "name" },
+  { label: "نام مرکزداده", id: "datacenter" },
+  { label: "وضعیت", id: "statusId" },
+  { label: "تاریخ ایجاد", id: "createDate" },
+  { label: "", id: "control" },
+];
+
