138db865bc079148efb4c8f61cab426faae6baff
Atefeh Masoumi <atefeh.masoomy@gmail.com>
2026-01-21 16:54:16 +0330

    fix: add new struct base on new api response

--- src/components/organisms/siem/siemList/struct.ts
+++ src/components/organisms/siem/siemList/struct.ts
@@ -2,7 +2,10 @@ import { columnType } from "../../tables/table.types";
 
 export const siemTableStruct: columnType[] = [
   { label: "نام  ", id: "name" },
-  { label: "نام مرکزداده", id: "datacenter" },
+  { label: "لاگ سیستم عامل", id: "osLogEnabled" },
+  { label: "لاگ سرویس ها ", id: "serviceLogEnabled" },
+  { label: "لاگ آنالیز ترافیک", id: "trafficAnalysisLogEnabled" },
+  { label: "لاگ IDS", id: "idsLogEnabled" },
   { label: "وضعیت", id: "statusId" },
   { label: "تاریخ ایجاد", id: "createDate" },
   { label: "", id: "control" },
