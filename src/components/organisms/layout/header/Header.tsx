8c1e57a92bce2199c2a191052d50779afe6ac562
Atefeh masoumi <a.masoumi@dorsa.cloud>
2026-01-28 13:51:30 +0330

    fix header bug after cherry pick

--- src/components/organisms/layout/header/Header.tsx
+++ src/components/organisms/layout/header/Header.tsx
@@ -163,9 +163,9 @@ const Header: FC<HeaderPropsType> = ({
       case BACK_URL_HINTS_ENUM.NETWORK_LIST:
         href = `/network/${projectId}`;
         break;
-      case BACK_URL_HINTS_ENUM.ADD_DEPLOYMENT:
-        href = `/kubernetes-cloud/${projectId}/${kubernetesCloudId}/deployment`;
-      break;
+      // case BACK_URL_HINTS_ENUM.ADD_DEPLOYMENT:
+      //   href = `/kubernetes-cloud/${projectId}/${kubernetesCloudId}/deployment`;
+      // break;
       case BACK_URL_HINTS_ENUM.ADD_BACKUPFROMVM:
         href = `/backup/${projectId}`;
       break;
